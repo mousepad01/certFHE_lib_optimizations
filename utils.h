ada0780a144323c69e11be6a84f87dff35ecab64
mousepad01 <acalin2000@gmail.com>
2021-07-15 09:31:09 +0300

    dependencies and mcros change

--- utils.h
+++ utils.h
@@ -1,11 +1,18 @@
 #define BIT(X) X & 0x01
 
+#ifndef MSVC_COMPILER_LOCAL_MACRO
+#define MSVC_COMPILER_LOCAL_MACRO (_MSC_VER && !__INTEL_COMPILER)
+#endif
+
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
 
 #include <immintrin.h>
+
+#ifdef MSVC_COMPILER_LOCAL_MACRO
 #include <intrin.h>
+#endif
 
 #include <random>
 
